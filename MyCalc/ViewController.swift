2e65b975f5548cfead4ede0eef40046ee30a860c
Nikolai Saganenko <9952728@gmail.com>
2021-10-11 20:17:14 +0300

    Make +- button, AC button and % button

--- MyCalc/ViewController.swift
+++ MyCalc/ViewController.swift
@@ -9,16 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var typingEnded: Bool = true
+    
     @IBOutlet weak var resultsLabel: UILabel!
     @IBAction func calcPressed(_ sender: UIButton) {
+        
+        typingEnded = true
+        let number = Double(resultsLabel.text!)!
+        
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "+/-" {
+                resultsLabel.text = String(number * -1)
+            }
+            else if calcMethod == "%" {
+                resultsLabel.text = String(number / 100)
+            }
+            else if calcMethod == "AC" {
+                resultsLabel.text = "0"
+            }
+        }
     }
     
     @IBAction func numPressed(_ sender: UIButton) {
+        
+        if let numButtonPressed = sender.currentTitle {
+            if typingEnded {
+                resultsLabel.text = numButtonPressed
+                typingEnded = false
+            } else {
+                resultsLabel.text! += numButtonPressed
+            }
+            
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
         
     }
 
